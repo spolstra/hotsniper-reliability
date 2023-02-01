45648cc390557efd8a909071423226d4174c12fe
Simon Polstra <s.polstra@uva.nl>
2023-02-01 17:03:29 +0100

    Added kelvin to celsius conversion script.

---
+++ convert_to_celsius.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) != 2:
+    print('usage: {} temperature-file-kelvin'.format(sys.argv[0]))
+    sys.exit(1)
+
+temperature_file = sys.argv[1]
+
+def k_to_c(k):
+    return k - 273.15
+
+with open(temperature_file) as f:
+    print(f.readline(), end='')  # Copy header
+    temperatures = list(map(float, f.readline().split('\t')))
+    temperatures = list(map(k_to_c, temperatures))
+    first = True
+    for t in temperatures:
+        if first:
+            first = False
+        else:
+            print('\t', end='')
+        print("{:.4}".format(t), end='')
+    print()
